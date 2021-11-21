2c7a24e9fdebc0f629b705e45c4b7af1a50e204c
phumthai <phumthai.spy@hotmail.com>
2021-11-21 22:28:45 +0700

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import App1 from './App1'
 import Nav from './Nav.js'
 
 ReactDOM.render(<Nav />, document.getElementById('root'));
